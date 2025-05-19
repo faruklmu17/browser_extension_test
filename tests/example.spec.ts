864010082c121bd40c19d68abcaf235072cb12c6
faruklmu17 <farukqmul@gmail.com>
2025-05-19 09:47:36 -0400

    6 tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,4 +8,9 @@ test('has title', async ({ page }) => {
 });
 
 
+test('has title1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
