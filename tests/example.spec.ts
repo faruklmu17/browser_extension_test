1d50b6a31ab9bf295d0458d5271e057f0438e129
faruklmu17 <farukqmul@gmail.com>
2025-05-21 19:16:46 -0400

    22 tesets

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -62,3 +62,17 @@ test('has title8', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+
+test('has title9', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('has title10', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
