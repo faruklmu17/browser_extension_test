9fe6e6e1cf09a46024a2b06e958062e7c30c1b2f
faruklmu17 <farukqmul@gmail.com>
2025-05-24 23:20:06 -0400

    4 failed

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -21,3 +21,10 @@ test('has title2', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("");
 });
+
+test('has title3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("");
+});
