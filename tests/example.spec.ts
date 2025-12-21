6353ca3732a7ace612a5ac967871814e8fa511c2
faruklmu17 <farukqmul@gmail.com>
2025-12-21 00:26:43 -0500

    syntax error test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -24,3 +24,9 @@ test('has title2', async ({ page }) => {
 });
 
 
+test('has title2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("");
+});
