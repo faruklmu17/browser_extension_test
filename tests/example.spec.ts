29377fe157932e693f556694149dc716435e9f25
faruklmu17 <farukqmul@gmail.com>
2026-05-23 08:43:13 -0400

    updated tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -33,14 +33,3 @@ test('search functionality is present', async ({ page }) => {
   const searchButton = page.getByRole('button', { name: /search/i });
   await expect(searchButton).toBeVisible();
 });
-test('saearch functionality is present', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const searchButton = page.getByRole('button', { name: /search/i });
-  await expect(searchButton).toBeVisible();
-});
-
-test('searcsh functionality is present', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const searchButton = page.getByRole('button', { name: /search/i });
-  await expect(searchButton).toBeVisible();
-});
