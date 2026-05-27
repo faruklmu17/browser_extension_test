787cb78151503234744a5508eb0e438bacb05af1
faruklmu17 <farukqmul@gmail.com>
2026-05-27 12:24:53 -0400

    updated code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -29,6 +29,17 @@ test('main heading is visible', async ({ page }) => {
 });
 
 
+test('API liaank is present in navigation', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const apiLink = page.getByRole('link', { name: 'A' });
+  await expect(apiLink).toBeVisible();
+});
 
+// Content tests
+test('main haeading is visible', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const heading = page.locator('h1').first();
+  await expect(heading).toBeVisible();
+});
 
 
